9ca0cc6639fe382009cc660b1113f2de4a366ec0
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-05 20:31:32 +0200

    Add route for creating redirects

---
+++ src/app/api/redirect/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { createRedirect } from '@/lib/redirect';
+
+export async function POST(req: Request) {
+	const { longUrl, shortUrl } = await req.json();
+
+	if (!longUrl || !shortUrl) {
+		return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+	}
+
+	console.log(longUrl, shortUrl);
+
+	const redirect = await createRedirect(longUrl, shortUrl);
+
+	if (redirect.error) {
+		return NextResponse.json({ error: redirect.error }, { status: redirect.status });
+	}
+
+	return NextResponse.json({ success: true }, { status: 201 });
+}
