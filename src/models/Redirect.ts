5982abe786973775e3889da918179c57f18ea6ed
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-05 20:31:39 +0200

    Refactor Redirect model to include creation and update dates

--- src/models/Redirect.ts
+++ src/models/Redirect.ts
@@ -2,15 +2,24 @@ import mongoose, { Document, now } from 'mongoose';
 
 export interface IRedirect extends Document {
 	_id: string;
-	date: Date;
+	creationDate: Date;
+	updateDate: Date;
 	slug: string;
 	url: string;
 }
 
 const redirectsSchema = new mongoose.Schema<IRedirect>({
-	date: { type: Date, required: true, default: now() },
+	creationDate: { type: Date, required: true, default: now },
+	updateDate: { type: Date, required: true, default: now },
 	slug: { type: String, required: true },
 	url: { type: String, required: true },
+});
+
+redirectsSchema.pre('save', function (next) {
+	if (this.isModified()) {
+		this.updateDate = now();
+	}
+	next();
 });
 
 export const RedirectModel = mongoose.models.Redirect || mongoose.model<IRedirect>('Redirect', redirectsSchema);
