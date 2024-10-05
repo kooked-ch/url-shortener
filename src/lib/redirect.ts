8b47465ca35a305181cd6083d625fc3cce960d6c
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-05 20:31:43 +0200

    Add createRedirect function to handle URL redirection

---
+++ src/lib/redirect.ts
@@ -0,0 +1,11 @@
+import { ErrorType } from '@/types/error';
+
+export async function createRedirect(longUrl: string, shortUrl: string): Promise<ErrorType> {
+
+    
+
+	return {
+		error: '',
+		status: 201,
+	};
+}
