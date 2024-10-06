a90cbae0d4eafd22bf45bd3160b68051fa3939bc
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-06 20:13:11 +0200

    Add init-mongo.js script to create a user in MongoDB

---
+++ init-mongo.js
@@ -0,0 +1,7 @@
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+
+db.createUser({
+	user: process.env.MONGO_INITDB_USER_USERNAME,
+	pwd: process.env.MONGO_INITDB_USER_PASSWORD,
+	roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }],
+});
