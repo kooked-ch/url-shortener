d88fe26ac4d3aefe5c4f17654d0b68779cee384e
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-06 20:13:29 +0200

    Add functionality to create a redirect in the redirect.ts file

--- src/lib/redirect.ts
+++ src/lib/redirect.ts
@@ -1,8 +1,34 @@
 import { ErrorType } from '@/types/error';
+import db from './mongo';
+import { RedirectModel } from '@/models/Redirect';
 
 export async function createRedirect(longUrl: string, shortUrl: string): Promise<ErrorType> {
+	await db.connect();
 
-    
+	// Check if short URL already exists
+	const existing = await RedirectModel.findOne({
+		slug: shortUrl,
+	});
+
+	if (existing) {
+		return {
+			error: 'Short URL already exists',
+			status: 400,
+		};
+	}
+
+	// Create new redirect
+	const redirect = await RedirectModel.create({
+		slug: shortUrl,
+		url: longUrl,
+	});
+
+	if (!redirect) {
+		return {
+			error: 'Error creating redirect',
+			status: 500,
+		};
+	}
 
 	return {
 		error: '',
