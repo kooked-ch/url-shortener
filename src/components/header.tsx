5491e7ddc1fa2ad5f153b575c03a91fd98e81e18
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-07 14:21:46 +0200

    Refactor Header component to use Nav component for displaying navigation links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,10 @@
 'use server';
+
 import { getServerSession } from 'next-auth';
-import { headers } from 'next/headers';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { Nav } from './nav';
 
 export async function Header() {
 	const session = await getServerSession();
-	const headerList = headers();
-	const pathname = headerList.get('x-current-path');
 
-	const isActive = (href: string) => (pathname === href ? 'underline' : '');
-
-	if (!session) {
-		return <></>;
-	}
-
-	return (
-		<header className="absolute top-2 w-full flex justify-center">
-			<nav className="flex items-center gap-4">
-				<Link href="/">
-					<Button className={isActive('/')} variant="link">
-						Short Url
-					</Button>
-				</Link>
-				<Link href="/my-short">
-					<Button className={isActive('/my-short')} variant="link">
-						My Short
-					</Button>
-				</Link>
-			</nav>
-		</header>
-	);
+	return <header className="absolute top-2 w-full flex justify-center">{session && <Nav />}</header>;
 }
