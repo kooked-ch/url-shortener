935d5f30a9fb40ce98aaed155e027f2c2c6244d8
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-07 16:21:55 +0200

    Refactor nav component to add CSS classes for active links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 export function Nav() {
 	const pathname = usePathname();
@@ -10,12 +11,12 @@ export function Nav() {
 	return (
 		<nav className="flex items-center gap-4">
 			<Link href="/">
-				<Button className={isActive('/')} variant="link">
+				<Button className={cn(isActive('/'), 'text-gray-500 hover:text-gray-600')} variant="link">
 					Short Url
 				</Button>
 			</Link>
 			<Link href="/my-short">
-				<Button className={isActive('/my-short')} variant="link">
+				<Button className={cn(isActive('/my-short'), 'text-gray-500 hover:text-gray-600')} variant="link">
 					My Short
 				</Button>
 			</Link>
