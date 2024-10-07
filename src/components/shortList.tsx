808e8ef2cea42c1b7af7d2c8550d48c39bef79aa
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-07 16:39:47 +0200

    Refactor shortList component to adjust styling and spacing

--- src/components/shortList.tsx
+++ src/components/shortList.tsx
@@ -1,9 +1,9 @@
 export function ShortList({ items }: { items: { url: string; slug: string; count: number }[] }) {
 	return (
-		<div className="flex flex-col gap-2 mt-16 w-1/5">
+		<div className="flex flex-col gap-3 mt-16 w-1/5">
 			<h1 className="text-3xl font-black text-center mb-2">Shorted URL</h1>
 			{items.map((item, index) => (
-				<div className="bg-muted rounded-md p-4 flex items-center justify-between" key={'short' + index}>
+				<div className="bg-muted rounded-md p-4 px-7 flex items-center justify-between" key={'short' + index}>
 					<div>
 						<div className="text-foreground font-medium">{item.slug}</div>
 						<div className="text-muted-foreground text-sm">{item.url}</div>
