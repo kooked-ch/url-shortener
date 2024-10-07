1761eb06912c680d532c8b2f1b01e6587c86accf
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-07 14:21:56 +0200

    Refactor MyShortPage component to use ShortList component for displaying a list of shortened URLs and add getRedirects function to redirect.ts

--- src/app/my-short/page.tsx
+++ src/app/my-short/page.tsx
@@ -1,6 +1,29 @@
+import { ShortList } from '@/components/shortList';
+import { getRedirects } from '@/lib/redirect';
 import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-export async function MyShortPage() {
+export default async function MyShortPage() {
 	const session = await getServerSession();
-	return <div>{JSON.stringify(session)}</div>;
+
+	if (!session) {
+		redirect('/login');
+	}
+
+	const redirects = await getRedirects(session?.user?.email || '');
+
+	if (redirects.length === 0) {
+		return (
+			<div className="flex justify-center w-full mt-24">
+				<h1 className="text-3xl font-black">URL Shortener</h1>
+				There are no redirects
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex justify-center w-full">
+			<ShortList items={redirects} />
+		</div>
+	);
 }
