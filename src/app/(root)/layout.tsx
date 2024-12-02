a3d24a19bb44e9e0e960db037bfeb02e224d97d7
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-02 16:49:38 +0100

    Add background class to RootLayout main element

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
-	return <main>{children}</main>;
+	return <main className="bg-background">{children}</main>;
 }
