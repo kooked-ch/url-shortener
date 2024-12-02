ab519e6ab1ce3e002163d8fd6d3d847d761df667
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-02 19:58:52 +0100

    Refactor MainPage component to include Header and Features components, replacing the ShortForm layout for improved structure and user experience

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,23 +1,20 @@
 'use server';
 
-import ShortForm from '@/components/forms/shortForm';
 import { Link } from 'lucide-react';
 import { getServerSession } from 'next-auth';
+import { UrlShortener } from '@/components/url-shortener';
+import { Header } from '@/components/header';
+import { Features } from '@/components/features';
 
 export default async function MainPage() {
 	const session = await getServerSession();
 	const baseUrl = process.env.NEXTAUTH_URL || '';
 
 	return (
-		<div className="container mx-auto px-4 py-12">
-			<div className="mb-12 text-center space-y-4">
-				<div className="flex items-center justify-center space-x-3">
-					<Link className="h-12 w-12 text-primary" />
-					<h1 className="text-5xl font-bold bg-gradient-to-r from-green-600 to-emerald-600 dark:from-green-400 dark:to-emerald-400 text-transparent bg-clip-text">Url Shortener</h1>
-				</div>
-				<p className="text-lg text-gray-600 dark:text-gray-400 max-w-2xl mx-auto">Easily transform long URLs into short, elegant, and shareable links.</p>
-			</div>
-			<ShortForm baseUrl={new URL(baseUrl).host} />;
+		<div className="w-full px-4 py-12 space-y-16">
+			<Header />
+			<UrlShortener baseUrl={new URL(baseUrl).host} />
+			<Features />
 		</div>
 	);
 }
