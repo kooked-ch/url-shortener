7e098f3de5bf6edfe6759bb977c6af486e3f03a7
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-02 17:06:23 +0100

    Add Header component to RootLayout for improved page structure

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,11 @@
+import { Header } from '@/components/header';
 import React, { ReactNode } from 'react';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
-	return <main className="bg-background">{children}</main>;
+	return (
+		<>
+			<Header />
+			<main className="bg-background">{children}</main>;
+		</>
+	);
 }
