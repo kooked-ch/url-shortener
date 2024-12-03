86479492bc7350aa81e79758e829ca4fccc91a0f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-03 22:04:55 +0100

    Update User model to make username and email optional, and add temporaryToken field

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,16 +9,18 @@ export interface IUser extends Document {
 	password?: string;
 	role: string;
 	verified?: boolean;
+	temporaryToken?: string;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
 	name: { type: String, required: false },
-	username: { type: String, required: true },
+	username: { type: String, required: false },
 	image: { type: String, required: false },
-	email: { type: String, required: true },
+	email: { type: String, required: false },
 	password: { type: String, required: false },
 	role: { type: String, required: false, default: 'member' },
 	verified: { type: Boolean, required: true, default: false },
+	temporaryToken: { type: String, required: false },
 });
 
 export const UserModel = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
