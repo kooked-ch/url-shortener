1d03e4db1c861bcf89c96aad3a29603ca6986703
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-03 22:04:48 +0100

    Enhance POST handler in redirect route to handle user creation and temporary tokens

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -1,24 +1,66 @@
 import { NextResponse } from 'next/server';
 import { createRedirect } from '@/lib/redirect';
-import { getServerSession, Session } from 'next-auth';
+import { getServerSession } from 'next-auth';
 import { UserModel } from '@/models/User';
+import { cookies } from 'next/headers';
+import db from '@/lib/mongo';
+
+function generateRandomString(length: number): string {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	return Array.from({ length }, () => characters.charAt(Math.floor(Math.random() * characters.length))).join('');
+}
 
 export async function POST(req: Request) {
-	const { longUrl, shortUrl } = await req.json();
+	try {
+		const { longUrl, shortUrl } = await req.json();
 
-	if (!longUrl || !shortUrl) {
-		return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+		if (!longUrl || !shortUrl) {
+			return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+		}
+
+		const session = await getServerSession();
+		const cookieStore = cookies();
+
+		let user = null;
+
+		await db.connect();
+
+		if (session?.user?.email) {
+			user = await UserModel.findOne({ email: session.user.email });
+		} else {
+			const cookieData = cookieStore.get('__Secure-next-auth.temporary');
+			const temporaryToken = cookieData?.value;
+
+			if (temporaryToken) {
+				user = await UserModel.findOne({ temporaryToken });
+			} else {
+				const token = generateRandomString(256);
+				user = await UserModel.create({
+					email: null,
+					username: null,
+					temporaryToken: token,
+				});
+
+				cookieStore.set('__Secure-next-auth.temporary', token, {
+					httpOnly: true,
+					secure: true,
+					sameSite: 'strict',
+				});
+			}
+		}
+
+		if (!user) {
+			return NextResponse.json({ error: 'User not found or could not be created' }, { status: 404 });
+		}
+
+		const redirect = await createRedirect(longUrl, shortUrl, user._id);
+		if (redirect.error) {
+			return NextResponse.json({ error: redirect.error }, { status: redirect.status });
+		}
+
+		return NextResponse.json({ success: true }, { status: 201 });
+	} catch (error) {
+		console.error('Error in POST handler:', error);
+		return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
 	}
-
-	const session: Session | null = await getServerSession();
-
-	const user = await UserModel.findOne({ email: session?.user?.email });
-
-	const redirect = await createRedirect(longUrl, shortUrl, user._id);
-
-	if (redirect.error) {
-		return NextResponse.json({ error: redirect.error }, { status: redirect.status });
-	}
-
-	return NextResponse.json({ success: true }, { status: 201 });
 }
