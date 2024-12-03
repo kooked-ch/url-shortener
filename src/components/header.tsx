a9d154bd5c370d67694f6da4804b22dfd13bcf37
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-03 22:55:35 +0100

    Enhance Header component with sign-in link for unauthenticated users and update URL Shortener title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 
 import { getServerSession, Session } from 'next-auth';
 import { Link2 } from 'lucide-react';
+import Link from 'next/link';
 
 export async function Header() {
 	const session: Session | null = await getServerSession();
@@ -9,9 +10,19 @@ export async function Header() {
 		<header className="text-center space-y-4">
 			<div className="flex items-center justify-center gap-2">
 				<Link2 className="h-12 w-12 text-emerald-500" />
-				<h1 className="text-4xl font-bold text-gray-900 dark:text-white">Url Shortener</h1>
+				<h1 className="text-4xl font-bold text-gray-900 dark:text-white">URL Shortener</h1>
 			</div>
-			<p className="text-xl text-gray-600 max-w-2xl mx-auto">Easily transform long URLs into short, elegant, and shareable links.</p>
+			<p className="text-xl text-gray-600 max-w-2xl mx-auto">
+				Transform long URLs into short, shareable links.{' '}
+				{session ? (
+					''
+				) : (
+					<Link href="/auth/signin" className="text-emerald-500 hover:underline">
+						Sign in
+					</Link>
+				)}{' '}
+				to update your links and access more details.
+			</p>
 		</header>
 	);
 }
