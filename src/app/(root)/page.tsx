5e7de46f41d66f82130703dd50adcf8e2382be5c
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-04 11:21:44 +0100

    Refactor MainPage function to remove unnecessary parameters and logging

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,11 +6,9 @@ import { Features } from '@/components/features';
 import { cookies } from 'next/headers';
 import { getTemporaryUserRedirects } from '@/lib/redirect';
 
-export default async function MainPage(req: Request) {
+export default async function MainPage() {
 	const baseUrl = process.env.NEXTAUTH_URL || '';
 	const cookieStore = await cookies();
-	console.log(cookieStore);
-	console.log(cookieStore.get('__Secure-next-auth.temporary'));
 	const baseUrls: string[] = await getTemporaryUserRedirects(cookieStore.get('__Secure-next-auth.temporary')?.value || null);
 
 	return (
