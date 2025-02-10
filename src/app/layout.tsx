73f885b916fe201e8f3ebc7791f02ef8a0c571fb
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-10 20:39:58 +0100

    Replace script tag with next/script component for Plausible analytics in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 
 import React from 'react';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,12 +26,12 @@ export default function RootLayout(props: RootLayoutProps) {
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<meta name="apple-mobile-web-app-title" content="MyWebSite" />
 				<link rel="manifest" href="/site.webmanifest" />
-				<script defer data-domain="go.kooked.app" src="https://plausible.io/js/script.js"></script>
 			</head>
 			<body className={inter.className}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 					{children}
 				</ThemeProvider>
+				<Script defer data-domain="go.kooked.app" src="https://plausible.io/js/script.js" />
 			</body>
 		</html>
 	);
